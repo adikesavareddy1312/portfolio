dcc5ec02aca8bf408c185bee0045a05aa600c185
Adikesavareddy <adhikesavareddy11318@gmail.com>
2025-01-02 13:21:59 +0530

    My portfolio project

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Simple JavaScript example to show alert when the "Contact" section is clicked
+document.getElementById("contact").addEventListener("click", function() {
+    alert("You clicked the contact section!");
+  });
+  
